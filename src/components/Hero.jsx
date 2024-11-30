08f2cabecec758c5731e17cf1762bc207878a4cf
rayhanbeg <mdrayhan40301@gmail.com>
2024-11-30 21:20:54 +0600

    resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import HeroImage from '../assets/hero-image.jpg'
+import pdf from '../assets/Rs_Fahim.pdf'
 
 const Hero = () => {
   return (
@@ -18,9 +19,13 @@ const Hero = () => {
             <a href='#contact'
             className='bg-gradient-to-r from-green-400 to-blue-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me</a>
-            <button
-            className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</button>
+            <a
+              href={pdf} 
+              download="Fahim_Resume.pdf"
+              className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
+              transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
+              Resume
+            </a>
         </div>
     </div>
   )
